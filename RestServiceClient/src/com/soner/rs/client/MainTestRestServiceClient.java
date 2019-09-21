ac7054245d2278cec20f31467f0e15425b0889b2
soners.ariusak <soner.sariusak@gmail.com>
2019-09-21 19:10:47 +0300

    -- duzenlemeler yapildi

--- RestServiceClient/src/com/soner/rs/client/MainTestRestServiceClient.java
+++ RestServiceClient/src/com/soner/rs/client/MainTestRestServiceClient.java
@@ -10,20 +10,26 @@ import com.sun.jersey.api.client.WebResource;
 public class MainTestRestServiceClient {
 
 	public static void main(String[] args) {
-		
-//		System.out.println("This is \"Post\" process..");
-//		post();
+
+		System.out.println("This is \"Post\" process..");
+		post();
 		System.out.println("This is \"Get\" process..");
 		get();
 	}
 
 	private static void get() {
-		Client client = Client.create();
-		WebResource resource = client.resource("http://localhost:8080/RestService/rest/orders/get");
-		ClientResponse response = resource.accept(MediaType.APPLICATION_XML).get(ClientResponse.class);
-		if (response.getStatus() == 200) {
-			System.out.println(response.getEntity(String.class));
+		try {
+			//A client was created using the Jersey Rest API.
+			Client client = Client.create();
+			WebResource resource = client.resource("http://localhost:8080/RestService/rest/orders/get");
+			ClientResponse response = resource.accept(MediaType.APPLICATION_XML).get(ClientResponse.class);
+			if (response.getStatus() == 200) {
+				System.out.println(response.getEntity(String.class));
+			}
+		} catch (Exception e) {
+			System.out.println("Exception is:" + e.getMessage());
 		}
+
 	}
 
 	private static void post() {
@@ -35,6 +41,7 @@ public class MainTestRestServiceClient {
 		order.setOrderNumber(2);
 
 		try {
+			//A client was created using the Jersey Rest API.
 			Client client = Client.create();
 			WebResource resource = client.resource("http://localhost:8080/RestService/rest/orders/post");
 			ClientResponse response = resource.type(MediaType.APPLICATION_XML).post(ClientResponse.class, order);
@@ -42,7 +49,7 @@ public class MainTestRestServiceClient {
 				System.out.println(response.getEntity(String.class));
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Exception is:" + e.getMessage());
 		}
 
 	}
--- RestServiceClient/src/com/soner/rs/client/model/Order.java
+++ RestServiceClient/src/com/soner/rs/client/model/Order.java
@@ -1,7 +1,5 @@
 package com.soner.rs.client.model;
 
-import java.time.LocalDate;
-
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
@@ -10,7 +8,6 @@ public class Order {
 	private int orderNumber;
 	private String orderName;
 	private String orderBuyer;
-	private LocalDate orderDate;
 	private float orderAmount;
 
 	public int getOrderNumber() {
@@ -37,21 +34,12 @@ public class Order {
 		this.orderBuyer = orderBuyer;
 	}
 
-
 	public float getOrderAmount() {
 		return orderAmount;
 	}
 
 	public void setOrderAmount(float orderAmount) {
 		this.orderAmount = orderAmount;
-	}
-
-	public LocalDate getOrderDate() {
-		return orderDate;
-	}
-
-	public void setOrderDate(LocalDate orderDate) {
-		this.orderDate = orderDate;
 	}
 
 }
